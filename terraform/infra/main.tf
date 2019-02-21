b1c816f5e2b357d9f7f882b10626074c77e45d99
azwickey-pivotal <azwickey@pivotal.io>
2019-02-21 08:54:40 -0500

    infra TF-ing

--- terraform/infra/main.tf
+++ terraform/infra/main.tf
@@ -30,3 +30,110 @@ resource "google_compute_subnetwork" "services" {
   region                   = "${var.region}"
   private_ip_google_access = "${var.internetless}"
 }
+
+// Allow ssh from public networks
+resource "google_compute_firewall" "allow-ssh" {
+  name    = "${var.env_name}-allow-ssh"
+  network = "${var.network_name}"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["allow-ssh"]
+}
+
+// Allow http from public
+resource "google_compute_firewall" "pcf-allow-http" {
+  name    = "${var.env_name}-allow-http"
+  network = "${var.network_name}"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["allow-http", "router"]
+}
+
+// Allow https from public
+resource "google_compute_firewall" "pcf-allow-https" {
+  name    = "${var.env_name}-allow-https"
+  network = "${var.network_name}"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["443"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["allow-https", "router"]
+}
+
+//// GO Router Health Checks
+resource "google_compute_firewall" "pcf-allow-http-8080" {
+  name    = "${var.env_name}-allow-http-8080"
+  network = "${var.network_name}"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["8080"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["router"]
+}
+
+//// Create Firewall Rule for allow-ert-all com between bosh deployed ert jobs
+//// This will match the default OpsMan tag configured for the deployment
+resource "google_compute_firewall" "allow-ert-all" {
+  name       = "${var.env_name}-allow-ert-all"
+  depends_on = ["google_compute_network.pcf-virt-net"]
+  network    = "${var.network_name}"
+
+  allow {
+    protocol = "icmp"
+  }
+
+  allow {
+    protocol = "tcp"
+  }
+
+  allow {
+    protocol = "udp"
+  }
+
+  target_tags = ["${var.env_name}", "${var.env_name}-opsman", "nat-traverse"]
+  source_tags = ["${var.env_name}", "${var.env_name}-opsman", "nat-traverse"]
+}
+
+//// Allow access to ssh-proxy [Optional]
+resource "google_compute_firewall" "cf-ssh-proxy" {
+  name       = "${var.env_name}-allow-ssh-proxy"
+  depends_on = ["google_compute_network.pcf-virt-net"]
+  network    = "${var.network_name}"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["2222"]
+  }
+
+  target_tags = ["${var.env_name}-ssh-proxy", "diego-brain"]
+}
+
+//// Allow access to Optional CF TCP router
+resource "google_compute_firewall" "cf-tcp" {
+  name       = "${var.env_name}-allow-cf-tcp"
+  depends_on = ["google_compute_network.pcf-virt-net"]
+  network    = "${var.network_name}"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["1024-65535"]
+  }
+
+  target_tags = ["${var.env_name}-cf-tcp-lb"]
+}
