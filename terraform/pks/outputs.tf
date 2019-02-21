a093721b6fa4dbc40e2ab1735719759008eb4de7
azwickey-pivotal <azwickey@pivotal.io>
2019-02-21 11:31:47 -0500

    pks TF-ing

--- terraform/pks/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "pks_master_node_service_account_key" {
-  value = "${base64decode(google_service_account_key.pks_master_node_service_account_key.private_key)}"
-}
-
-output "pks_worker_node_service_account_key" {
-  value = "${base64decode(google_service_account_key.pks_worker_node_service_account_key.private_key)}"
-}
