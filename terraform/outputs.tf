d50d71bf8382fde99bcebf45c883874b75b29854
azwickey-pivotal <azwickey@pivotal.io>
2019-02-21 11:33:20 -0500

    pks TF-ing

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -59,9 +59,9 @@ output "services_ip_cidr_range" {
 }
 
 output "pks_master_node_service_account_key" {
-  value = "${base64decode(google_service_account_key.pks_master_node_service_account_key.private_key)}"
+  value = "${base64decode(module.pks.google_service_account_key.pks_master_node_service_account_key.private_key)}"
 }
 
 output "pks_worker_node_service_account_key" {
-  value = "${base64decode(google_service_account_key.pks_worker_node_service_account_key.private_key)}"
+  value = "${base64decode(module.pks.google_service_account_key.pks_worker_node_service_account_key.private_key)}"
 }
