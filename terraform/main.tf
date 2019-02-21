8991e3731970e28a9a2c85ae57c225e7a2193a5a
azwickey-pivotal <azwickey@pivotal.io>
2019-02-20 21:15:39 -0500

    infra TF-ing

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,7 @@
 provider "google" {
   project     = "${var.project}"
   region      = "${var.region}"
-  credentials = "${var.service_account_key}"
-
+  credentials = "${file("${var.service_account_key}")}"
   version = ">= 1.7.0"
 }
 
