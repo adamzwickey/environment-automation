28f7e442018bb23dc0beab0bd4055f65ce88e868
azwickey-pivotal <azwickey@pivotal.io>
2019-02-21 10:40:44 -0500

    infra TF-ing

--- terraform/infra/main.tf
+++ terraform/infra/main.tf
@@ -70,7 +70,7 @@ resource "google_compute_firewall" "allow-pcf-all" {
   # Allow HTTP/S access to Ops Manager from the outside world
   resource "google_compute_firewall" "ops-manager-external" {
     name        = "${var.env_name}-ops-manager-external"
-    network     = "${var.pcf_network_name}"
+    network     = "${var.network_name}"
     target_tags = ["${var.env_name}-ops-manager-external"]
 
     allow {
