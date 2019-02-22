28989ba1f57064e510c5fa7482265730e9a055bc
azwickey-pivotal <azwickey@pivotal.io>
2019-02-22 14:17:02 -0500

    infra TF-ing

--- bin/bosh-bootstrap-login.sh
+++ bin/bosh-bootstrap-login.sh
@@ -1,6 +1,5 @@
-bosh int ../creds/bosh-bootstrap-creds.yml --path /director_ssl/ca > state/bosh-bootstrap-ca
-bosh alias-env bosh-bootstrap -e $CONTROL_PLANE_FQDNS --ca-cert state/bosh-bootstrap-ca
+bosh alias-env bosh-bootstrap -e $CONTROL_PLANE_FQDNS
 export BOSH_CLIENT=admin
-export BOSH_CLIENT_SECRET=$(bosh int ../creds/bosh-bootstrap-creds.yml --path /admin_password)
+export BOSH_CLIENT_SECRET=$(bosh int ./creds/bosh-bootstrap-creds.yml --path /admin_password)
 export BOSH_ENVIRONMENT=bosh-bootstrap
 bosh login
---
+++ bin/credhub-login.sh
@@ -0,0 +1,7 @@
+bosh int ./creds/bosh-bootstrap-creds.yml --path /credhub_tls/certificate > state/credhub_ca_cert
+export CREDHUB_CA=state/credhub_ca_cert
+export CREDHUB_PWD=$(bosh int ./creds/bosh-bootstrap-creds.yml --path /credhub_cli_user_password)
+export CREDHUB_USER=credhub_cli_user
+export CREDHUB_SERVER=https://$CONTROL_PLANE_FQDNS:8844
+credhub api $CREDHUB_SERVER --ca-cert=$CREDHUB_CA
+credhub login --ca-cert=$CREDHUB_CA -u $CREDHUB_USER -p $CREDHUB_CLIENT_PWD
