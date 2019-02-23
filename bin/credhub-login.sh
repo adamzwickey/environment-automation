caf05afe906b7e2f8553fc26837f66e1e044f8a5
azwickey-pivotal <azwickey@pivotal.io>
2019-02-22 21:40:56 -0500

    pipeline for opsman deploy

--- bin/credhub-login.sh
+++ bin/credhub-login.sh
@@ -5,4 +5,3 @@ export CREDHUB_USER=credhub_cli_user
 export CREDHUB_SERVER=https://$CONTROL_PLANE_FQDNS:8844
 credhub api $CREDHUB_SERVER --ca-cert=$CREDHUB_CA
 credhub login --ca-cert=$CREDHUB_CA -u $CREDHUB_USER -p $CREDHUB_PWD
-credhub --version
