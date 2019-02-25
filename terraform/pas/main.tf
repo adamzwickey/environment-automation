cd9d8857084d4b0333d1a29bb97b999a3d3da8b0
azwickey-pivotal <azwickey@pivotal.io>
2019-02-25 14:55:47 -0500

    pipeline for paving PAS

--- terraform/pas/main.tf
+++ terraform/pas/main.tf
@@ -75,28 +75,28 @@ resource "google_compute_firewall" "cf-tcp" {
 resource "google_storage_bucket" "buildpacks" {
   name          = "${var.project}-${var.env_name}-buildpacks"
   location      = "${var.buckets_location}"
-  force_destroy = true
+  force_destroy = false
   count         = "${var.create_gcs_buckets ? 1 : 0}"
 }
 
 resource "google_storage_bucket" "droplets" {
   name          = "${var.project}-${var.env_name}-droplets"
   location      = "${var.buckets_location}"
-  force_destroy = true
+  force_destroy = false
   count         = "${var.create_gcs_buckets ? 1 : 0}"
 }
 
 resource "google_storage_bucket" "packages" {
   name          = "${var.project}-${var.env_name}-packages"
   location      = "${var.buckets_location}"
-  force_destroy = true
+  force_destroy = false
   count         = "${var.create_gcs_buckets ? 1 : 0}"
 }
 
 resource "google_storage_bucket" "resources" {
   name          = "${var.project}-${var.env_name}-resources"
   location      = "${var.buckets_location}"
-  force_destroy = true
+  force_destroy = false
   count         = "${var.create_gcs_buckets ? 1 : 0}"
 }
 
