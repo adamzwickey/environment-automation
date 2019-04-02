37e1a9e933dda65126914a5ec58cc9cdfe652413
azwickey-pivotal <azwickey@pivotal.io>
2019-04-02 15:04:03 -0400

    added task to automate cluster creation

--- tasks/bootstrap-cluster/task.sh
+++ tasks/bootstrap-cluster/task.sh
@@ -8,3 +8,6 @@ export CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)"
 echo "deb http://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 sudo apt-get update && sudo apt-get install google-cloud-sdk
+
+gcloud auth activate-service-account --key-file=credential_key.json
+gcloud config set project $GCP_PROJECT_ID
