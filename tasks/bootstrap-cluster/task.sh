536ad5a6b9a78fe2492868fce39af446953b69c3
azwickey-pivotal <azwickey@pivotal.io>
2019-04-03 10:02:08 -0400

    updated to xenial rootfs

--- tasks/bootstrap-cluster/task.sh
+++ tasks/bootstrap-cluster/task.sh
@@ -4,9 +4,9 @@ set -eu
 
 export ROOT_DIR=`pwd`
 export CLOUD_SDK_REPO="cloud-sdk-xenial"
-echo "deb http://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
-curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-sudo apt-get update -y && sudo apt-get install -y google-cloud-sdk
+echo "deb http://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+apt-get update -y && apt-get install -y google-cloud-sdk
 
 gcloud auth activate-service-account --key-file=credential_key.json
 gcloud config set project $GCP_PROJECT_ID
