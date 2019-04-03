4b511077333da2873df25bb087ff24067342e2a1
azwickey-pivotal <azwickey@pivotal.io>
2019-04-03 10:06:07 -0400

    updated to xenial rootfs

--- tasks/bootstrap-cluster/task.sh
+++ tasks/bootstrap-cluster/task.sh
@@ -2,6 +2,8 @@
 # Source: https://github.com/pivotalservices/concourse-pipeline-samples/blob/master/tasks/pcf/pks/configure-pks-cli-user/task.sh
 set -eu
 
+apt-get update -y && apt-get install -y curl
+
 export ROOT_DIR=`pwd`
 export CLOUD_SDK_REPO="cloud-sdk-xenial"
 echo "deb http://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
