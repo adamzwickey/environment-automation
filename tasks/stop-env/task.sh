e462ab3714b2100cb5ccd8979f47a7ce5f591d46
azwickey-pivotal <azwickey@pivotal.io>
2019-04-03 12:57:11 -0400

    adding code to stop all deployments

--- tasks/stop-env/task.sh
+++ tasks/stop-env/task.sh
@@ -1,11 +1,57 @@
 #!/bin/bash
 # Source: https://github.com/pivotalservices/concourse-pipeline-samples/blob/master/tasks/pcf/pks/configure-pks-cli-user/task.sh
 set -eu
-
 export ROOT_DIR=`pwd`
 
+curl -L $BOSH_CLI_URL -o $ROOT_DIR/bosh
+chmod u+x $ROOT_DIR/bosh
+
+curl -L $OM_CLI_URL -o $ROOT_DIR/om
+chmod u+x $ROOT_DIR/om
+
+eval "$($ROOT_DIR/om \
+       -t https://$OPSMAN_DOMAIN_OR_IP_ADDRESS \
+       -u $OPSMAN_USERNAME \
+       -p $OPSMAN_PASSWORD \
+       --skip-ssl-validation \
+       bosh-env)"
+$ROOT_DIR/bosh login
+
 # Stop On-Demand SIs and PKS clusters
+service_instances=($($ROOT_DIR/bosh -e pcf deployments | grep service-instance | awk '{print $1}'))
+echo "Stopping Service Instances:"
+length=${#service_instances[@]}
+for ((i = 0; i != length; i++)); do
+   echo "$i: ${service_instances[i]}"
+   $ROOT_DIR/bosh stop --hard -d ${service_instances[i]}
+done
 
 # Stop everything BUT cf
+deployments=($($ROOT_DIR/om \
+              -t https://$OPSMAN_DOMAIN_OR_IP_ADDRESS \
+              -u $OPSMAN_USERNAME \
+              -p $OPSMAN_PASSWORD \
+              --skip-ssl-validation \
+              curl -p /api/v0/deployed/products \
+              2>/dev/null \
+              | jq -rc '.[] .guid' \
+              grep -v "cf-"))
+echo "Stopping all deployments but CF:"
+length=${#deployments[@]}
+for ((i = 0; i != length; i++)); do
+   echo "Stopping $i: ${deployments[i]}"
+   $ROOT_DIR/bosh stop --hard -d ${deployments[i]}
+done
 
 # Stop cf
+cf_deployment=($($ROOT_DIR/om \
+              -t https://$OPSMAN_DOMAIN_OR_IP_ADDRESS \
+              -u $OPSMAN_USERNAME \
+              -p $OPSMAN_PASSWORD \
+              --skip-ssl-validation \
+              curl -p /api/v0/deployed/products \
+              2>/dev/null \
+              | jq -rc '.[] .guid' \
+              grep "cf-"))
+echo "Stopping CF deployment: $cf_deployment"
+$ROOT_DIR/bosh stop --hard -d ${cf_deployment}
