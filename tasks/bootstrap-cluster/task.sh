a99015adb2b5564631fa7c438f64a98ab834f516
azwickey-pivotal <azwickey@pivotal.io>
2019-04-03 09:21:22 -0400

    updated bootstrap rootfs

--- tasks/bootstrap-cluster/task.sh
+++ tasks/bootstrap-cluster/task.sh
@@ -7,7 +7,7 @@ export ROOT_DIR=`pwd`
 export CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)"
 echo "deb http://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-sudo apt-get update && sudo apt-get install google-cloud-sdk
+sudo apt-get update -y && sudo apt-get install -y google-cloud-sdk
 
 gcloud auth activate-service-account --key-file=credential_key.json
 gcloud config set project $GCP_PROJECT_ID
