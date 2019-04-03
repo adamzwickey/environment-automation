ec5209636b563fc33ed8a0653c464da5898dd9eb
azwickey-pivotal <azwickey@pivotal.io>
2019-04-03 13:24:52 -0400

    adding code to stop all deployments

--- tasks/stop-env/task.sh
+++ tasks/stop-env/task.sh
@@ -25,7 +25,7 @@ echo "Stopping Service Instances:"
 length=${#service_instances[@]}
 for ((i = 0; i != length; i++)); do
    echo "$i: ${service_instances[i]}"
-   $ROOT_DIR/bosh stop --hard -d ${service_instances[i]}
+   $ROOT_DIR/bosh -n stop --hard -d ${service_instances[i]}
 done
 
 # Stop everything BUT cf
@@ -42,7 +42,7 @@ echo "Stopping all deployments but CF:"
 length=${#deployments[@]}
 for ((i = 0; i != length; i++)); do
    echo "Stopping $i: ${deployments[i]}"
-   $ROOT_DIR/bosh stop --hard -d ${deployments[i]}
+   $ROOT_DIR/bosh stop -n --hard -d ${deployments[i]}
 done
 
 # Stop cf
@@ -54,6 +54,18 @@ cf_deployment=($($ROOT_DIR/om \
               curl -p /api/v0/deployed/products \
               2>/dev/null \
               | jq -rc '.[] .guid' \
-              | grep "cf-"))
-echo "Stopping CF deployment: $cf_deployment"
-$ROOT_DIR/bosh stop --hard -d ${cf_deployment}
+              | grep "cf-" \
+              | wc -l ))
+if [ "$cf_deployment" == "1" ]; then
+  cf=($($ROOT_DIR/om \
+                -t https://$OPSMAN_DOMAIN_OR_IP_ADDRESS \
+                -u $OPSMAN_USERNAME \
+                -p $OPSMAN_PASSWORD \
+                --skip-ssl-validation \
+                curl -p /api/v0/deployed/products \
+                2>/dev/null \
+                | jq -rc '.[] .guid' \
+                | grep "cf-" ))
+  echo "Stopping CF deployment: $cf"
+  $ROOT_DIR/bosh stop -n --hard -d ${cf}
+fi
